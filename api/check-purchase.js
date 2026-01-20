d1380c9da6bb149706a8185ff0b9a25f11813a4c
nameusermc <michael.cardin@hotmail.com>
2026-01-20 17:27:43 -0500

    Create check-purchase.js

---
+++ api/check-purchase.js
@@ -0,0 +1,7 @@
+// Test serverless function
+export default function handler(req, res) {
+  res.status(200).json({ 
+    message: 'API is working!',
+    timestamp: new Date().toISOString()
+  });
+}
